30e4cdbeb1e62e683b0fde98de21e8392256c30d
Luis Gustavo de Souza Xavier <luisgustavoxavier1234@gmail.com>
2025-06-23 00:25:54 -0300

    estrutura inicial da classe produtos

--- LuisGustavo/produtos.cpp
+++ LuisGustavo/produtos.cpp
@@ -1,4 +1,50 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+class Produtos{
+    private:
+        string nome;
+        int codigo;
+        int quantidade;
+        float preco;
+    public:
+        void setNome(string n){
+            nome = n;
+        }
+        void setQuantidade(int q){
+            quantidade = q;
+        }
+        void setPreco(float p){
+            preco = p;
+        }
+        void gerarCodigo(){
+            unsigned seed = time(0);
+            srand(seed);
+            codigo = rand()%1000;
+        }
+        Produtos(string nome, float preco){
+            setNome(nome);
+            setPreco(preco);
+            setQuantidade(quantidade);
+            gerarCodigo();
+        }
+        string getNome(){
+            return nome;
+        }
+
+        int getCodigo(){
+            return codigo;
+        }
+        int getQuantidade(){
+            return quantidade;
+        }
+        float getPreco(){
+            return preco;
+        }
+
+
+
 
 int main(){}
