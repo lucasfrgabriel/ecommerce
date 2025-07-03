080fe0e95372e67fb2cc2890d28a46dfb35ce728
Lucas Gabriel de Oliveira Franco <lucasfrgabriel@gmail.com>
2025-07-03 00:10:07 -0300

    ??

--- Lucas/vendedores.hpp
+++ Lucas/vendedores.hpp
@@ -53,9 +53,6 @@ class Vendedores{
     float getComissoes(){return comissoes;}
 
     void salvarNoArquivo(ofstream &arquivo) {
-        if (numero < 0) {
-            numero = rand()%1000;
-        }
         arquivo << "-------------------------------------------------------------------------------------------------------------------------\n";
         arquivo << "Nome: " << nome << " - numero: " << numero << " - salario: " << salario << " - comissoes: " << comissoes<< "\n";
         arquivo << "-------------------------------------------------------------------------------------------------------------------------\n";
